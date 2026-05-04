642718301050c5bdf24240ff67d7c17fe8aded54
P. Isulu <isulupeter@gmail.com>
2026-05-04 20:58:40 +0200

    feat(customForm): Moved the button display to be from the customfrom to the modal content decoupling it from the pristine state.

--- src/components/saveConfiguration/ModalContent.tsx
+++ src/components/saveConfiguration/ModalContent.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form } from 'react-final-form';
 import { ModalContentInterface } from '../../types/modal/ModalProps';
 import { CustomForm, WithBorder, WithPadding } from 'dhis2-semis-components';
-import { LinearProgress } from '@mui/material';
+import { Button, LinearProgress } from '@mui/material';
 import { useRecoilValue } from 'recoil';
 import { ProgramLoaderState } from '../../atoms/getProgramLoaderSchema';
+import { ButtonStrip } from "@dhis2/ui";
 
 function ModalContent(props: ModalContentInterface) {
     const { formFields, onSubmit, onCancel, initialValues, loading, setTrackedValues, extraContent } = props;
     const loadingProgram = useRecoilValue<boolean>(ProgramLoaderState)
+    const [currentValues, setCurrentValues] = useState<Record<string, any>>(initialValues ?? {})
+
+    const handleSetTrackedValues = (values: any)    => {
+        setCurrentValues(values)
+        setTrackedValues?.(values)
+    }
 
     return (
         <WithPadding>
@@ -18,15 +25,33 @@ function ModalContent(props: ModalContentInterface) {
                     <CustomForm
                         Form={Form}
                         loading={loading}
-                        withButtons={true}
+                        withButtons={false}
                         formFields={formFields}
-                        setTrackedValues={setTrackedValues}
+                        setTrackedValues={handleSetTrackedValues}
                         initialValues={initialValues}
                         onCancel={() => { onCancel() }}
                         trackedEntity={initialValues?.trackedEntity}
                         onFormSubtmit={(e: Record<string, any>) => { onSubmit(e) }}
                     />
                     {extraContent}
+                    <ButtonStrip end>
+                        <Button
+                          onClick={() => { onCancel() }}
+                          color="secondary"
+                          disabled={!!loading}
+                          variant={"outlined"}
+                        >
+                            Cancel
+                        </Button>
+                        <Button
+                          onClick={() => { onSubmit(currentValues) }}
+                          disabled={!!loading}
+                          color="primary"
+                          variant={"contained"}
+                        >
+                            Save
+                        </Button>
+                    </ButtonStrip>
                 </WithPadding>
             </WithBorder>
         </WithPadding>
